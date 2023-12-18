40c9a15c7a5d71a1b9e2e8ba524cc5553643285b
JoshuaArgiropoulos <joshua.argiropoulos@gmail.com>
2023-12-18 14:06:22 -0500

    Massive advancements, email to user works and confirming user in DB works. Creating user also works and can be seen in the mySQL db

--- webApp.py
+++ webApp.py
@@ -2,13 +2,15 @@ from itsdangerous import URLSafeTimedSerializer
 import plotly.graph_objects as go
 import os
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, render_template, redirect, url_for, flash
-from werkzeug.security import generate_password_hash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+import mysql.connector
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_mail import Mail, Message
 from flask_wtf import CSRFProtect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, EqualTo
 
-
-#app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('config.py')
 mail = Mail(app)
@@ -21,7 +23,35 @@ if os.environ.get('DATABASE_URI'):
 
 
 db = SQLAlchemy(app)
+# Token Serializer Initialization
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255))
+    is_active = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        InputRequired(),
+        EqualTo('password', message='Passwords must match.')
+    ])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Log In')
+
 # Your code to create the dynamic chart
 def create_chart():
     chart_path = 'static/images/chart.html'
@@ -47,13 +77,29 @@ def services():
 def about():
     return render_template('about.html')
 
-@app.route('/SignOn')  
+@app.route('/SignOn', methods=['GET', 'POST'])
 def sign_on():
-    return render_template('SignOn.html')
+    form = LoginForm()
+    if form.validate_on_submit():  # This checks both POST method and form validation
+        # Query database for user
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        # Check if user exists and password is correct
+        if user and check_password_hash(user.password_hash, form.password.data):
+            # You can add a session or any other login mechanism here
+            session['user_id'] = user.id
+            # Redirect to a different page on successful login
+            return redirect(url_for('dashboard'))  # Ensure you have a 'dashboard' route defined
+        else:
+            # Flash a message if login details are incorrect
+            flash('Invalid username or password')
+
+    return render_template('SignOn.html', form=form)
 
 @app.route('/SignUp')
 def sign_up():
-    return render_template('SignUp.html')
+    form = RegistrationForm()
+    return render_template('SignUp.html', form=form)
 
 @app.route('/stock_info')
 def stock_info():
@@ -68,44 +114,81 @@ def stock_info():
     }
     return render_template('stock_info.html', stock_data=stock_data)
 
+@app.route('/dashboard')
+def dashboard():
+    # Ensure user is logged in, otherwise redirect to login page
+    if 'user_id' not in session:
+        return redirect(url_for('sign_on'))
+    return render_template('dashboard.html')
+
 #---------------------------Mail Features -------------------------------------------------------
 
 @app.route('/register', methods=['POST'])
 def register():
-    # Extract data from form
-    username = request.form.get('username')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    confirm_password = request.form.get('confirm_password')  # Make sure to add this to your form
+    form = RegistrationForm(request.form)
+    if form.validate_on_submit():
+        # Extract data from form
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
 
-    if password != confirm_password:
-        flash('Passwords do not match.')
-        return redirect(url_for('sign_up'))
+        # Check if user already exists
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user is not None:
+            flash('A user with that email already exists.')
+            return redirect(url_for('sign_up'))
 
+        # Hash the password
+        hashed_password = generate_password_hash(password)
 
-    # Password hashing
-    hashed_password = generate_password_hash(password)
+        # Create new user and save to database
+        new_user = User(username=username, email=email, password_hash=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
 
-    flash('Registration successful! Please check your email.', 'success')
-    # Redirect to a different page after successful registration, e.g., login page
-    return redirect(url_for('sign_on'))
+        # Send confirmation email
+        token = s.dumps(new_user.id, salt='email-confirm-salt')
+        send_confirmation_email(email, token)
+
+        flash('Registration successful! Please check your email to confirm your account.', 'success')
+        return redirect(url_for('sign_on'))
+    else:
+        # Handle validation errors
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash("Error in {}: {}".format(
+                    getattr(form, field).label.text, error
+                ))
+
+    # If validation fails, redirect back to the sign-up page
+    return redirect(url_for('sign_up'))
     
 
     
-    # Process registration form
+    
     
 def send_confirmation_email(user_email, token):
-
-@app.route('/confirm_email/<token>')
-def confirm_email(token):
-    # Verify the token
-    # Activate the user's account if the token is valid
-    # ...
-    return 'Email confirmed successfully!'
-    msg = Message('Confirm Your Email', sender='your_email@example.com', recipients=[user_email])
+    msg = Message('Confirm Your Email', sender='ThriveOnFinance', recipients=[user_email])
     confirm_url = url_for('confirm_email', token=token, _external=True)
     msg.body = 'Your confirmation link is: {}'.format(confirm_url)
     mail.send(msg)
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        user_id = s.loads(token, salt='email-confirm-salt', max_age=3600)  # 1 hour expiration
+    except:
+        return 'The confirmation link is invalid or has expired.'
+
+    user = User.query.get(user_id)
+    if user:
+        user.is_active = True
+        db.session.commit()
+        return 'Email confirmed successfully!'
+    else:
+        return 'User not found'
+    
+  
 # ------------------------------------------------------------------------
 
 if __name__ == '__main__':
