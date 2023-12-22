7405e54fb3a1cb8519c1870c30475cdf9eb058ea
JoshuaArgiropoulos <Joshua.argiropoulos@gmail.com>
2023-12-22 02:11:45 -0500

    React is driving me crazy, using flask + html has the nav broken and react has the drop down broken, will sleep on it

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
-import '../assests/styles/styles.css'; // Include your CSS styles
+import '../assests/styles/styles.css'; // Ensure the path is correct
+
 function Navigation() {
   const [dropdownActive, setDropdownActive] = useState(false);
 
   const toggleDropdown = () => {
-    setDropdownActive(!dropdownActive);
+    setDropdownActive(!dropdownActive); // Toggles the dropdownActive state
   };
 
   return (
     <nav style={{ backgroundColor: '#0066cc', padding: '1em', textAlign: 'center' }}>
-      <a href="/">Home</a>
-      <div className="dropdown" onClick={toggleDropdown}>
-        <a href="#" className="dropdown-btn">Services</a>
-        {dropdownActive && (
-          <div className="dropdown-content">
+      <a href="/" className="nav-link">Home</a>
+      <div className="dropdown">
+        <button onClick={toggleDropdown} className="dropdown-btn nav-link">Services</button>
+        <div className={`dropdown-content ${dropdownActive ? 'active' : ''}`}>
+
             <a href="/Financial-News">Financial News</a>
             <a href="/interactive-tools">Interactive Tools</a>
             <a href="/investment">Investment Education</a>
             <a href="/investment-education">Stock Information</a>
           </div>
-        )}
+    
       </div>
-      <a href="/about">About Us</a>
-      <a href="/SignOn">Sign On</a>
+      <a href="/about" className="nav-link">About Us</a>
+      <a href="/SignOn" className="nav-link">Sign On</a>
     </nav>
   );
 }
-
 export default Navigation;
