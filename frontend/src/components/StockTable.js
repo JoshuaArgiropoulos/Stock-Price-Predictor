12eeaeb0c2ede97eabe0712e0d7c192ecef10b23
JoshuaArgiropoulos <Joshua.argiropoulos@gmail.com>
2023-12-24 18:12:40 -0500

    Saving the stocks for now

--- frontend/src/components/StockTable.js
+++ frontend/src/components/StockTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -28,6 +28,13 @@ function StockTable() {
   const [chartData, setChartData] = useState(null);
   const [error, setError] = useState(null);
   const [sinceInception, setSinceInception] = useState(false);
+
+   // useEffect for clearing the start date when 'Since Inception' is checked
+   useEffect(() => {
+    if (sinceInception) {
+      setStartDate(''); // Clear the start date
+    }
+  }, [sinceInception]); // Only re-run the effect if sinceInception changes
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -170,138 +177,3 @@ function StockTable() {
 
 export default StockTable;
 
-// import React, { useState } from 'react';
-// import { Line } from 'react-chartjs-2';
-// import 'chartjs-adapter-date-fns';
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-//   TimeScale,
-//   TimeSeriesScale
-// } from 'chart.js';
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-//   TimeScale,
-//   TimeSeriesScale // Ensure you're registering the correct scale type
-// );
-
-// function StockTable() {
-//   const [ticker, setTicker] = useState('');
-//   const [startDate, setStartDate] = useState('');
-//   const [endDate, setEndDate] = useState('');
-//   const [chartData, setChartData] = useState(null);
-//   const [error, setError] = useState(null);
-//   const [sinceInception, setSinceInception] = useState(false);
-
-//   // Convert date string to timestamp
-//   function formatDateToTimestamp(dateString) {
-//     return new Date(dateString).getTime();
-//   }
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setError(null);
-
-//     if (!ticker || (!sinceInception && !startDate) || !endDate) {
-//       setError("Please fill in all fields");
-//       return;
-//     }
-//     const effectiveStartDate = startDate;
-
-//     try {
-//       const response = await fetch(`/api/stock-info/${ticker}?start_date=${effectiveStartDate}&end_date=${endDate}`);
-//       if (response.status === 404) {
-//         setError("Stock not found");
-//         setChartData(null);
-//         return;
-//       }
-//       if (!response.ok) {
-//         throw new Error(`Network response was not ok: ${response.status}`);
-//       }
-
-//       const responseData = await response.json();
-
-//       if (responseData.error) {
-//         throw new Error(responseData.error);
-//       }
-
-//       // Extract and convert date and closing price from the API response
-//       const timestamps = responseData.data.map((item) => formatDateToTimestamp(item.Date));
-//       const closingPrices = responseData.data.map((item) => item.Close);
-
-//       // Update chartData with the extracted data
-//       setChartData({
-//         datasets: [{
-//           label: `Closing Price for ${ticker}`,
-//           data: closingPrices.map((price, index) => ({ x: timestamps[index], y: price })),
-//           fill: false,
-//           borderColor: 'rgba(75, 192, 192, 1)',
-//         }],
-//         options: {
-//           scales: {
-//             x: {
-//               type: 'time',
-//               time: {
-//                 tooltipFormat: 'll',
-//               },
-//               ticks: {
-//                 autoSkip: true,
-//                 maxTicksLimit: 10,
-//               },
-//             },
-//           },
-//           plugins: {
-//             tooltip: {
-//               callbacks: {
-//                 label: function(context) {
-//                   const label = context.dataset.label || '';
-//                   const value = context.parsed.y;
-//                   const date = new Date(context.parsed.x).toLocaleDateString(); // Convert timestamp back to readable date
-//                   return `${label}: ${value}, Date: ${date}`;
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       });
-
-//     } catch (err) {
-//       setError(`Failed to fetch: ${err.message}`);
-//       setChartData(null);
-//     }
-//   };
-
-//   return (
-//     <div className="HomePage" style={{ textAlign: 'center' }}>
-//       <div>
-//         <h2>Stock Chart</h2>
-//         <form onSubmit={handleSubmit}>
-//           {/* ... rest of your form ... */}
-//         </form>
-//       </div>
-
-//       {error && <p>Error: {error}</p>}
-
-//       {chartData && (
-//         <div>
-//           <Line data={chartData} options={chartData.options} />
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default StockTable;
