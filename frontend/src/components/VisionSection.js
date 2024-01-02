a8af1575823028689d89e3d754072ae91ea9ff2e
JoshuaArgiropoulos <Joshua.argiropoulos@gmail.com>
2024-01-01 19:59:40 -0500

    Fixed home page, look into stock loading speed. Takes very long

--- frontend/src/components/VisionSection.js
+++ frontend/src/components/VisionSection.js
@@ -8,7 +8,7 @@ function VisionSection() {
         <h3 className={styles.title}>Empowering Futures</h3>
         <p>Embark on a transformative financial journey with ThriveOnFinance. We're dedicated to empowering your financial growth and stability.</p>
         <p>Every dollar counts, and we understand its value. Let us help you maximize your financial potential, ensuring that every decision moves you closer to your goals. Discover how to make your money work harder for you.</p>
-        <button className={styles.ctaButton}>Learn More</button> 
+        <a href="/signup" className={styles.ctaButton}>Sign Up</a>
       </div>
  
 </section>
