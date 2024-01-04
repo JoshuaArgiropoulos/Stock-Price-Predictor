e77e0a94e7f50a38a610c079fe66c9742aca6ac9
JoshuaArgiropoulos <Joshua.argiropoulos@gmail.com>
2024-01-04 16:18:12 -0500

    Save point before i create pasta code. All code works, attempting to move user data around when logged in

--- frontend/src/Dashboard/UserProfile.js
+++ frontend/src/Dashboard/UserProfile.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-
-function UserProfile() {
+import DashNav from './DashNav';
+import DashHeader from './DashHeader';
+function UserProfile(userData) {
   const location = useLocation();
   const user = location.state?.user;  // Access user from the route's state
 
   return (
     <div className="user-profile">
+      <DashHeader />
+        <DashNav user={userData}/>
       <h1>User Information</h1>
       <div>
         <p>Username: {user?.username}</p> 
